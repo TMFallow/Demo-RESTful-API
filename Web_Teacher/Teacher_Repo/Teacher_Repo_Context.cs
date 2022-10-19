366643b2f9a0481503968a58cbcb3d55df656585
Phan Thanh Bình <thanhbinh07102001@gmail.com>
2022-10-19 15:55:28 +0700

    Create Another Asp Net Core of Teacher and implement CRUD on it

---
+++ Web_Teacher/Teacher_Repo/Teacher_Repo_Context.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Teacher_Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teacher_Repo
+{
+    public class Teacher_Repo_Context : DbContext
+    {
+        public Teacher_Repo_Context(DbContextOptions<Teacher_Repo_Context> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            new Teacher_Map(modelBuilder.Entity<Teacher>());
+
+            //modelBuilder.ApplyConfigurationsFromAssembly(typeof(Student_Repo_Context).Assembly);
+        }
+
+        public DbSet<Teacher> Teachers { get; set; }
+    }
+}
---
+++ Web_Teacher/Web_Teacher/Controllers/TeacherController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Teacher_Data;
+using Teacher_Service;
+using Web_Teacher.Models;
+
+namespace Web_Teacher.Controllers
+{
+    public class TeacherController : Controller
+    {
+        private readonly ITeacher_Service teacher_Service;
+
+        public TeacherController(ITeacher_Service teacher_Service)
+        {
+            this.teacher_Service = teacher_Service;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var model = teacher_Service.GetAllTeachers();
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Addteacher()
+        {
+            TeacherViewModel model = new TeacherViewModel();
+            return View("Addteacher", model);
+        }
+
+        [HttpPost]
+        public IActionResult Addteacher(TeacherViewModel model)
+        {
+            Teacher teach = new Teacher
+            {
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                DateOfBirth = model.DateOfBirth,
+            };
+
+            teacher_Service.Insert(teach);
+            if (teach.Id > 0)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult EditTeacher(int? Id)
+        {
+            //StudentViewModel model = new StudentViewModel();
+            //if(Id.HasValue && Id.Value >0)
+            //{
+            Teacher std = teacher_Service.GetTeacher(Id);
+            //    model.Name = std.Name;
+            //    model.Classroom = std.Classroom;
+            //    model.DateOfBirth = std.DateOfBirth;
+            //}
+            return View("EditTeacher", std);
+        }
+
+        [HttpPost]
+        public IActionResult EditTeacher(TeacherViewModel model)
+        {
+            Teacher std = teacher_Service.GetTeacher(model.Id);
+            std.FirstName = model.FirstName;
+            std.LastName = model.LastName;
+            std.DateOfBirth = model.DateOfBirth;
+            teacher_Service.Update(std);
+            if (std.Id > 0)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult DeleteTeacher(int? Id)
+        {
+            Teacher student = teacher_Service.GetTeacher(Id);
+            return View("DeleteTeacher", student);
+        }
+
+        [HttpPost]
+        public IActionResult DeleteTeacher(int? Id, TeacherViewModel model)
+        {
+            Teacher std = teacher_Service.GetTeacher(Id);
+            teacher_Service.Delete(std);
+            return RedirectToAction("Index");
+        }
+    }
+}
